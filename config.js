da5c3ec35ad88876ec4680453e2bc36711654673
₿lueMoon <96875502+BlueMoon0x69@users.noreply.github.com>
2026-02-17 15:35:50 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+/**
+ * config.js - Configuration de votre collection
+ * ⚠️ Ce fichier ne doit être modifié qu'une seule fois !
+ * ⚠️ Ne partagez jamais ce fichier publiquement.
+ */
+
+const CONFIG = {
+    SHEET_ID: '1NYdX5dbtBqYI0Jw9AAuXibAUw95lraH_78wxirBfPrM',
+    API_KEY:  'AIzaSyCprd-1_qAoGBWqGMYd0dpB5KDvpdWgv3I',
+    SHEET_NAME: 'collection_pokemon'
+};
